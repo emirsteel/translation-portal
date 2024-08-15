f62aa9d3c3947a00dfd85f8ba839a3ea434f2e94
Emir Çelik <107912411+emirsteel@users.noreply.github.com>
2024-08-15 19:00:36 +0300

    Add files via upload

---
+++ frontend/src/pages/TranslationPage.js
@@ -0,0 +1,14 @@
+// src/pages/TranslationPage.js
+import React from 'react';
+import Translate from '../components/Translation/translate';
+
+function TranslationPage() {
+    return (
+        <div>
+            <h1>Translation Page</h1>
+            <Translate />
+        </div>
+    );
+}
+
+export default TranslationPage;
